eaa63419512507d5939f667f9547f66618a832a3
rohitgupta24 <rohit@allps.ch>
2021-03-23 11:22:44 +0530

    socket

--- network.py
+++ network.py
@@ -1,11 +1,12 @@
 import socket
 class Network:
     def __init__(self):
-        self.client = socket.socket(socket.IF_INET, socket.SOCK_STREAM)
-        self.server = ""
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.29.224"
         self.port = 5555
         self.addr = (self.server ,self.port)
-        self.connect()
+        self.id = self.connect()
+        print(self.id)
 
     def connect(self):
         try:
@@ -13,3 +14,15 @@ class Network:
             return self.client.recv(2048).decode()
 
         except:
+            pass
+
+    def send(self,data):
+        try:
+            self.client.send(str.encode(data))
+            return self.client.recv(2048).decode()
+        except socket.error as e:
+            print(e)
+
+n = Network()
+print(n.send("hello"))
+print(n.send("working"))
--- server.py
+++ server.py
@@ -5,17 +5,19 @@ import sys
 server = "192.168.29.224"
 port = 5555
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM )
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-try :
+try:
     s.bind((server, port))
 except socket.error as e:
     str(e)
 
 s.listen(2)
-print("Waiting for Connection, Server Started")
+print("Waiting for a connection, Server Started")
+
 
 def threaded_client(conn):
+    conn.send(str.encode("Connected"))
     reply = ""
     while True:
         try:
@@ -26,19 +28,19 @@ def threaded_client(conn):
                 print("Disconnected")
                 break
             else:
-                print("Received :", reply)
-                print("Sending :", reply)
+                print("Received: ", reply)
+                print("Sending : ", reply)
 
             conn.sendall(str.encode(reply))
-
         except:
             break
 
-    print("Lost Connection")
-    conn.close()        
+    print("Lost connection")
+    conn.close()
+
 
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(threaded_client,(conn,))
+    start_new_thread(threaded_client, (conn,))
